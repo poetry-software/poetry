c4951b68d4e48bca64ce0c0682dfb320dabcf82f
Alec DuBois <after.alec@gmail.com>
2025-08-03 17:12:48 -0700

    Initial commit

---
+++ demo/pwi/resources/js/pages/welcome.tsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome() {
+  return (
+    <>
+      <Head title="Welcome" />
+      <div>{/* Welcome page content will go here */}</div>
+    </>
+  );
+}
---
+++ demo/pwi/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
